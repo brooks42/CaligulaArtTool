d74f4609dcb1bac8b673c4f9094b8b0f8c919ecd
Chris Brooks <britainsux@gmail.com>
2018-06-10 20:38:57 -0700

    now you can run it to export paired layers, need to have it export metadata but that shouldn't be hard once the metadata info is defined

--- caligula.js
+++ caligula.js
@@ -10,10 +10,25 @@
 
 const program = require('commander');
 const execSync = require('child_process').execSync;
+const fs = require('fs');
 
 const metadataFileEnding = '.monstermeta';
 const lineEnding = '_line';
 const colorEnding = '_color';
+
+
+// class that stores our file metadata, including original PSD name, folder location, layer names etc
+class FileMetadata {
+
+	constructor(filename) {
+		this.filename = filename;
+		this.layerNames = [];
+	}
+}
+
+String.prototype.endsWith = function (s) {
+	return this.length >= s.length && this.substr(this.length - s.length) == s;
+}
 
 // TODO: add a flag for recursion
 // TODO: add a flag for location to store the [psd_name] folder at
@@ -25,9 +40,15 @@ program
 
 // here we decide which action to take based on command parameters
 if (program.folder !== undefined) {
+
+	// exports to a folder named "finals" inside the targeted folder
 	processFolder(program.folder);
 } else if (program.psd !== undefined) {
-	processFile(program.psd);
+
+	// exports to a file named file_name
+	var fileMetadata = new FileMetadata(program.psd);
+	// console.log()
+	processFile(fileMetadata);
 } else {
 	console.log('fell through');
 	processFolder('.');
@@ -43,20 +64,74 @@ function processFolder(folder) {
 // processes the passed file location
 // separates the layers out, compresses layer pairs (*_line and *_color) together and saves them
 // in a subfolder with the PSD name, with a metadata file
-function processFile(file) {
-	console.log(`Caligula processing file ${file}`);
+function processFile(fileMetadata) {
+	console.log(`Caligula processing file ${fileMetadata.filename}`);
 
 	// get the layer names from the PSD file
-	var layerNames = getLayerNames(file);
-	console.log(`got ${JSON.stringify(layerNames)} layers`);
+	fileMetadata.layerNames = getLayerNames(fileMetadata.filename);
+	console.log(`got ${JSON.stringify(fileMetadata.layerNames)} layers`);
 
 	// compress layer pairs to start building out the CLI
 	
 	// call the CLI to perform layer merging and exporting to PNG
 
 	// create a folder (in . for now) that's the PSD name
+	// TODO: ugh, just append this to the target directory when that's included in the params too...
+	var folderName = fileMetadata.filename.split('/')[fileMetadata.filename.split('/').length - 1].split('.')[0];
+	console.log(`Creating folder ${folderName}`);
+	
+	try {
+		fs.mkdirSync(folderName);
+	} catch (err) {
+		if (err.code != 'EEXIST') {
+			console.log(`got an error ${err} creating the folder ${folderName}, stopping here`);
+		}
+	}
 
-	// export the PNGs to that folder
+	var pairIndexes = []; // indexes of each pair, in pairs
+	var nonPairedLayerNames = []; // layers that we didn't detect a pair for
+
+	var lineIndex = 0;
+	var colorIndex = 0;
+
+	// iterate through the layer names to figure out pairs and their indexes
+	for (var potPairLine of fileMetadata.layerNames) {
+		if (potPairLine.endsWith('_line')) {
+			var line = potPairLine.split('_line')[0];
+			var paired = false;
+
+			for (var potPairColor of fileMetadata.layerNames) {
+				if (potPairColor.endsWith('_color')) {
+					console.log(`checking ${potPairColor}==${line + '_color'}`);
+					if (potPairColor == (line + '_color')) {
+						console.log(`paired layers ${line}`);
+						paired = true;
+						pairIndexes.push(lineIndex, colorIndex);
+						break;
+					}
+				}
+				colorIndex++;
+			}
+
+			colorIndex = 0;
+			if (!paired) {
+				nonPairedLayerNames.push(potPairLine);
+			}
+		} else {
+			if (potPairLine.endsWith('_color'))
+			nonPairedLayerNames.push(potPairLine);
+		}
+
+		lineIndex++;
+	}
+
+	// for each pair (each set that is the same except for _line and _color at the end), call the compositing function
+	for (var pairIndex = 0; pairIndex < pairIndexes.length - 1; pairIndex += 2) {
+		exportCompositeLayerAsPng(fileMetadata, `${folderName}/head.png`, pairIndexes[pairIndex], pairIndexes[pairIndex + 1]);
+	}
+
+	// track non-pair layers so we can store them to a file in the root 
+	console.log(`Non-paired layers: ${JSON.stringify(nonPairedLayerNames)}`);
 
 	// save a metadata file with the sizes of each PNG in it
 }
@@ -66,16 +141,21 @@ function getLayerNames(file) {
 	// the layer names come in from the command convert test/tank_sketch.psd -verbose info: | grep "label:" 
 	var cliReturn = execSync(`convert test/tank_sketch.psd -verbose info: | grep "label:" `);
 	
-	var tempLayersArray = cliReturn.toString().trim().split('label:');
-	var layersArray = [];
-
-	// because of CLI formatting we get an extraneous item and there's still some string cleanup to do here
-	for (item in tempLayersArray) {
-		if (item == 0) {
-			continue;
-		}
-		layersArray[item - 1] = tempLayersArray[item].trim();
+	var layersArray = cliReturn.toString().trim().split('label:');
+	for (index in layersArray) {
+		layersArray[index] = layersArray[index].trim();
 	}
 
 	return layersArray;
+}
+
+// exports the passed layer indexes as a single PNG 
+function exportCompositeLayerAsPng(fileMetadata, targetFilename, layer1Index, layer2Index) {
+	// this composites layers:
+	// magick composite -gravity center 'test/hair_1.psd[2]' 'test/hair_1.psd[1]' hair.png 
+	
+	console.log(`Exporting '${fileMetadata.filename}[${layer1Index}]' '${fileMetadata.filename}[${layer2Index}]' to ${targetFilename}`);
+	console.log(`CLI call is: magick composite -gravity center '${fileMetadata.filename}[${layer1Index}]' '${fileMetadata.filename}[${layer2Index}]' ${targetFilename}`);
+	var cliReturn = execSync(`magick composite -gravity center '${fileMetadata.filename}[${layer1Index}]' '${fileMetadata.filename}[${layer2Index}]' ${targetFilename}`);
+
 }
